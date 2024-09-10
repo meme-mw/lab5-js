b0336c8e9603a214b87565244817342ba6fb2166
unknown <madaawadh1@gmail.com>
2024-09-10 11:42:46 +0300

    lab6

--- js.js
+++ js.js
@@ -31,3 +31,77 @@ console.log(User.address);
 console.log(Object.values(User.address.city)[0]);
 console.log(User.skills[2]);
 console.log(User.skills.join(","));
+
+characters = [
+    {
+      name: "Luke Skywalker",
+      height: 177,
+      gender: "male",
+      mass: 77,
+      eye_color: "brown",
+    },
+    {
+      name: "Leia Organa",
+      height: 160,
+      gender: "female",
+      mass: 56,
+      eye_color: "blue",
+    },
+    {
+      name: "Han Solo",
+      height: 180,
+      gender: "male",
+      mass: 80,
+      eye_color: "brown",
+    },
+    {
+      name: "Chewie",
+      height: 222,
+      gender: "male",
+      mass: 190,
+      eye_color: "black",
+    },
+    {
+      name: "kevien",
+      height: 106,
+      gender: "male",
+      mass: 32.2,
+      eye_color: "red",
+    },
+  ];
+
+  let person =characters.find((person)=>person.eye_color==="blue");
+  console.log(person.name);
+  let person1 =characters.find((person)=>person.mass>50);
+  console.log(person1.gender);
+  const result = characters.filter((person) => person.height<200);
+
+  
+console.log(result);
+
+const result2 = characters.filter((person) => person.gender==="male");
+
+  
+console.log(result2);
+
+const result3 = characters.map((person) => person["name"]);
+
+  
+console.log(result3);
+
+const result4 = characters.map((person) => person["height"]);
+
+  
+console.log(result4);
+
+
+console.log(characters.sort((a, b) => a.mass - b.mass));
+
+console.log(characters.sort((a, b) => a.height - b.height).reverse());
+
+console.log(characters.every((person)=>person.mass>40));
+console.log(characters.every((person)=>person.height>200));
+
+console.log(characters.some((person)=>person.eye_color==="blue"));
+
+console.log(characters.some((person)=>person.height>210));
